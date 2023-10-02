ec0d56a1373c66f323ee02128f2cd9a8ca849b6f
redrobin97 <dylon20325@gmail.com>
2023-10-02 00:45:22 -0400

    final submission

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
-aert("\"Hello There\"-Ewan Mcgregor")
+alert("\"Hello There\"-Ewan Mcgregor")
 
 console.log('5')
 console.log('10')
+
+function addComment() {
+    var name = document.getElementById("name").value;
+    var comment = document.getElementById("comment").value;
+    var commentsList = document.getElementById("commentsList");
+  
+    var newComment = document.createElement("p");
+    newComment.textContent = name + ": " + comment;
+  
+    commentsList.appendChild(newComment);
+  
+    document.getElementById("name").value = "";
+    document.getElementById("comment").value = "";
+  }
